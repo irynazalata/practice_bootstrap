9038c8126874aba54450ef9334b08e31a855a40a
Iryna Zalata <iryna.zalata@gmail.com>
2020-12-17 18:11:27 +0200

    Added modal

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { Container, Navbar, Nav, Button, Modal, Form } from 'react-bootstrap';
 
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+  const onClose = () => setShow(false);
+  const onOpen = () => setShow(true);
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -25,7 +28,7 @@ const NavBar = () => {
               </Nav.Link>
             </Nav>
             <Nav className="mr-auto">
-              <Button variant="primary" className="mr-2">
+              <Button onClick={onOpen} variant="primary" className="mr-2">
                 Sign In
               </Button>
               <Button variant="primary" className="mr-2">
@@ -35,6 +38,37 @@ const NavBar = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <Modal show={show} onHide={onClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registration</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name, please" />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email, please"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password, please"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Control type="checkbox" placeholder="I agree" />
+              <Form.Text>I agree</Form.Text>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
